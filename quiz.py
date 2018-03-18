8188d774aa2ed599074b693d5210e1b6a4939f0d
Liam Cafearo <lcafearo@gmail.com>
2018-03-18 17:10:32 +0000

    added error catch

--- quiz.py
+++ quiz.py
@@ -1,6 +1,13 @@
+import sys
+
+
 def get_questions():
-    with open('questions.txt') as f:
-        lines = f.readlines()
+    try:
+        with open('wrong.txt') as f:
+            lines = f.readlines()
+    except:
+        print 'Questions file not found.'
+        sys.exit()
     return [(lines[i], lines[i+1].strip()) for i in range (0, len(lines), 2)]
 
 questions = get_questions()
