a64ad4c29546db70cc2c9662f45909b531562cc5
Liam Cafearo <lcafearo@gmail.com>
2018-03-18 17:03:58 +0000

    ammended get_questions function

--- quiz.py
+++ quiz.py
@@ -1,4 +1,13 @@
-f = open('questions.txt')
-lines = f.readlines()
-f.close()
-print lines
+def get_questions():
+    with open('questions.txt') as f:
+        lines = f.readlines()
+    return [(lines[i], lines[i+1].strip()) for i in range (0, len(lines), 2)]
+
+questions = get_questions()
+score = 0
+total = len(questions)
+for question, answer in questions:
+    guess = raw_input(question)
+    if guess == answer:
+        score += 1
+print 'You got %s out of %s questions right' % (score, total)
