d1363682f5b717ea60f9f9cb5f49dddf0b3d98e5
Liam Cafearo <lcafearo@gmail.com>
2018-03-18 17:33:16 +0000

    added more exceptions

--- quiz.py
+++ quiz.py
@@ -3,12 +3,23 @@ import sys
 
 def get_questions():
     try:
-        with open('wrong.txt') as f:
+        with open('questions.txt') as f:
             lines = f.readlines()
     except:
         print 'Questions file not found.'
         sys.exit()
     return [(lines[i], lines[i+1].strip()) for i in range (0, len(lines), 2)]
+
+
+try:
+    questions = get_questions()
+except IOError:
+    print 'Error reading questions file: %s' % e
+    sys.exit()
+except IndexError:
+    print 'Error: All questions in the questions file must have answers.'
+    sys.exit()
+
 
 questions = get_questions()
 score = 0
